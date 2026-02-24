8dcd75c659ae8ebd551e04db9a8521e889899de1
SiGMan <sigman@ioupg.com>
2026-02-24 00:40:44 -0800

    Cleanup oldies

--- components/font/include/font.h
+++ components/font/include/font.h
@@ -17,28 +17,6 @@
 void font_init(void);
 
 /**
- * Draw a single character
- *
- * @param x X position (pixels)
- * @param y Y position (pixels)
- * @param ch Character to draw
- * @param fg Foreground color
- * @param bg Background color
- */
-void font_draw_char(int x, int y, char ch, color_t fg, color_t bg);
-
-/**
- * Draw a string
- *
- * @param x X position (pixels)
- * @param y Y position (pixels)
- * @param str String to draw
- * @param fg Foreground color
- * @param bg Background color
- */
-void font_draw_string(int x, int y, const char *str, color_t fg, color_t bg);
-
-/**
  * Get font glyph bitmap
  *
  * @param ch Character
